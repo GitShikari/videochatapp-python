88b562c93383bdab6b4fdf6f0753d082916fa043
GitShikari <pro327644@gmail.com>
2024-09-07 09:51:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('main.html')
+
+@app.route('/room/<room_name>')
+def room(room_name):
+    return render_template('room.html', room_name=room_name)
+
+@app.route('/create_room', methods=['POST'])
+def create_room():
+    user_name = request.form['name']
+    room_name = f"room-{user_name}"  # Unique room name based on user name
+    return render_template('invite.html', room_name=room_name)
 
 @socketio.on('signal')
 def handle_signal(data):
-    emit('signal', data, broadcast=True, include_self=False)
+    room_name = data.get('room')
+    if room_name:
+        emit('signal', data, room=room_name)
+    else:
+        emit('signal', data, broadcast=True, include_self=False)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
